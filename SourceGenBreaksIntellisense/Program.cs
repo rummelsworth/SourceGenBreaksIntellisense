8fac90a7b7cb96f4d2eaaa33ec89f89ba9085396
William Rummler <w.a.rummler@gmail.com>
2021-06-08 17:09:56 -0500

    Add project files.

---
+++ SourceGenBreaksIntellisense/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SourceGenBreaksIntellisense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HelloWorldGenerated.HelloWorld.SayHello();
+        }
+
+        public Test test { get; set; }
+
+        public Test2 test2 { get; set; }
+    }
+
+    class Test2
+    {
+    }
+}
